0575a20003fca707c0a182c46c3396065bd7e30f
Jopel Simarmata <iss23036@students.del.ac.id>
2025-11-04 11:27:03 +0700

    add login with tailwind

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+    
 <!--
   This example requires updating your template:
 
@@ -44,3 +54,7 @@
     </p>
   </div>
 </div>
+
+
+</body>
+</html>
