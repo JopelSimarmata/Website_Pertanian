f47b7310b143e0ea2d268101c3912c4c0bfe4c08
Jopel Simarmata <iss23036@students.del.ac.id>
2025-11-06 16:02:05 +0700

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,16 @@ Route::get('/', function () {
     return view('pages.home');
 })->name('home');
 
-Route::get('/register',[AuthController::class, 'showRegister'])->name('show.register');
-Route::get('/login',[AuthController::class, 'showLogin'])->name('show.login');
-Route::post('/register',[AuthController::class, 'register'])->name('register');
-Route::post('/login',[AuthController::class, 'login'])->name('login');
 Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register',[AuthController::class, 'showRegister'])->name('show.register');
+    Route::get('/login',[AuthController::class, 'showLogin'])->name('show.login');
+    Route::post('/register',[AuthController::class, 'register'])->name('register');
+    Route::post('/login',[AuthController::class, 'login'])->name('login');
+});
+
+
 
 
 
