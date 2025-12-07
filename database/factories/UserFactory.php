36f69566c122a99cefa01d0ab48c57640116a227
Jopel Simarmata <iss23036@students.del.ac.id>
2025-12-08 05:43:22 +0700

    fix route

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,8 +28,29 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => fake()->randomElement(['petani', 'tengkulak']),
             'remember_token' => Str::random(10),
         ];
+    }
+
+    /**
+     * Indicate that the user is a petani (farmer).
+     */
+    public function petani(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'petani',
+        ]);
+    }
+
+    /**
+     * Indicate that the user is a tengkulak (middleman).
+     */
+    public function tengkulak(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'tengkulak',
+        ]);
     }
 
     /**
