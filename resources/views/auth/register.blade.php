40534798ff2ef039c1d4cf654ca3f67d3b651e80
Jopel Simarmata <iss23036@students.del.ac.id>
2025-12-08 18:18:58 +0700

    Test Deployment

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -97,6 +97,7 @@
                 @enderror
               </div>
 
+              
               <div>
                 <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
                 <input 
